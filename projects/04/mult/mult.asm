b0f6bfd5e4b07733e07281e6f9c2f84deb15655f
Oktarini Wandan Sari <oktarini7@gmail.com>
2017-08-14 18:48:00 +0700

    Project4: Multiplying 2 numbers loaded to RAM using HACK Machine Language

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,34 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@R0
+D=M
+@r0
+M=D //moving the value of R0 to a safe place
+@R1
+D=M
+@r1
+M=D //moving the value of R1 to a safe place
+@i
+M=0 //initialize i
+@R2
+M=0 //initialize mult
+(LOOP)
+	@i
+	D=M //check which iteration we are currently on
+	@r1
+	D=D-M
+	@END
+	D;JEQ
+	@r0
+	D=M
+	@R2
+	M=D+M
+	@i
+	M=M+1
+	@LOOP
+	0; JMP
+(END)
+	@END
+	0;JMP
+
