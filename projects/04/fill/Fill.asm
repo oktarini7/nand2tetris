e888118d26f286fa9f5335b92994a85fb2bbddc3
Oktarini Wandan Sari <oktarini7@gmail.com>
2017-08-16 13:44:08 +0700

    Project4: Probe the keyboard to change the screen color

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,44 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(MAIN_LOOP)
+@SCREEN
+D=A //beginning of screen
+@a //a for address needs to be modified
+M=D
+@KBD
+D=M //save the input from keyboard
+@LOOP_ON
+D;JNE // if keyboard has input then jump
+	(LOOP_OFF)
+		@SCREEN
+		D=A
+		@8191
+		D=A+D
+		@a
+		D=M-D
+		@MAIN_LOOP
+		D;JGT //if all pixels are white then stop and probe the keyboard again
+		@a
+		A=M
+		M=0 //whiten the screen
+		@a
+		M=M+1 //moving on to the next register
+		@LOOP_OFF
+		0;JMP
+	(LOOP_ON)
+		@SCREEN
+		D=A
+		@8191
+		D=A+D
+		@a
+		D=M-D
+		@MAIN_LOOP
+		D;JGT //if all pixels are black then stop and probe the keyboard again
+		@a
+		A=M
+		M=-1 //blacken the screen
+		@a
+		M=M+1 //moving on to the next register
+		@LOOP_ON
+		0;JMP
